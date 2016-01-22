6ac46ffb5d31d800375f21a00c21ca6b005f26fc
Lucas Caton <lucascaton@gmail.com>
2016-01-22 15:01:25 +1000

    Moving default_tools.yml to config folder

--- lib/massa/tool.rb
+++ lib/massa/tool.rb
@@ -6,7 +6,7 @@ module Massa
       end
 
       def tools_yaml_file
-        "#{Gem::Specification.find_by_name('massa').gem_dir}/lib/massa/default_tools.yml"
+        "#{Gem::Specification.find_by_name('massa').gem_dir}/config/default_tools.yml"
       rescue Gem::LoadError
         Massa::CLI.colorize :red, "¯\\_(ツ)_/¯ 'massa' gem is not in your Gemfile."
         exit 1
