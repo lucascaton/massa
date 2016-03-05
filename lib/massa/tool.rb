e79ba17aad95de6e8d97c8ecdae12e2189c409af
Lucas Caton <lucascaton@gmail.com>
2016-03-05 18:34:52 +1000

    Improving custom configs support

--- lib/massa/tool.rb
+++ lib/massa/tool.rb
@@ -4,13 +4,25 @@ module Massa
   class Tool
     class << self
       def list
-        default_tools = YAML.load_file(config_file_from_gem)
-
-        if File.exist?(config_file_from_project)
-          default_tools.merge(YAML.load_file(config_file_from_project) || {})
-        else
-          default_tools
+        default_tools.each_with_object({}) do |(tool, options), hash|
+          hash[tool] = options.merge(custom_tools[tool] || {})
+          hash
         end
+      end
+
+      private
+
+      def default_tools
+        YAML.load_file(config_file_from_gem)
+      end
+
+      def custom_tools
+        # Returns an empty hash if config file is empty
+        YAML.load_file(config_file_from_project) || {}
+
+      # When there is no config file in the project
+      rescue Errno::ENOENT
+        {}
       end
 
       def config_file_from_gem
