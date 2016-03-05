0b6602e1039b2df65ba03382165d913187e727a0
Lucas Caton <lucascaton@gmail.com>
2016-03-05 16:33:23 +1000

    Fixing 'Tool#list' method when custom config file is not present

--- lib/massa/tool.rb
+++ lib/massa/tool.rb
@@ -4,7 +4,13 @@ module Massa
   class Tool
     class << self
       def list
-        YAML.load_file(config_file_from_gem).merge YAML.load_file(config_file_from_project)
+        default_tools = YAML.load_file(config_file_from_gem)
+
+        if File.exist?(config_file_from_project)
+          default_tools.merge YAML.load_file(config_file_from_project)
+        else
+          default_tools
+        end
       end
 
       def config_file_from_gem
